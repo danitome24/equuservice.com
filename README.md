c9078ba1a5ed3ac582f4b64266f9ec7d659eab08
Daniel Tomé Fernández <danieltomefer@gmail.com>
2024-11-06 23:31:51 +0100

    Initial commit for equuservice.com

---
+++ README.md
@@ -0,0 +1,3 @@
+## Equuservice.com
+
+Website of equuservice.
---
+++ src/components/sections/services/data.ts
@@ -0,0 +1,59 @@
+import img1 from "./assets/solar3.png";
+import img2 from "./assets/wind.png";
+// TODO
+import img3 from "./assets/hydro.png";
+import img4 from "./assets/storage.png";
+// TODO
+import img5 from "./assets/smart-grid.png";
+import img6 from "./assets/green.png";
+
+export const servicesSectionData = {
+  title: "Nuestros servicios",
+  subheading: {
+    text1_1: "Un Transporte - ",
+    text1_2: "Diseñado para una total comodidad.",
+    text2:
+      "El mejor transporte de caballos de Tarragona.",
+  },
+  services: [
+    {
+      title: "Solar Power Solutions",
+      briefDescription:
+        "Harness the power of the sun with our state-of-the-art solar panels. Generate clean energy for your home or business and reduce your carbon footprint.",
+      visual: img1,
+    },
+    {
+      title: "Wind Energy Harvesting",
+      briefDescription:
+        "Embrace the power of the wind. Our wind energy solutions provide a sustainable and efficient way to generate electricity, ensuring a greener future.",
+      visual: img2,
+    },
+    {
+      title: "Hydropower Systems",
+      briefDescription:
+        "Flow with nature. Our hydropower systems harness the energy of flowing water, delivering reliable and eco-friendly electricity to your doorstep.",
+      visual: img3,
+    },
+    {
+      title: "Energy Storage Solutions",
+      briefDescription:
+        "Store excess energy efficiently. Our cutting-edge energy storage solutions ensure uninterrupted power supply, even during cloudy days or low-wind periods.",
+      visual: img4,
+    },
+    {
+      title: "Smart Grid Technology",
+      briefDescription:
+        "Navigate the future with our smart grid technology. Optimize energy distribution, monitor consumption, and contribute to a more sustainable and efficient energy ecosystem.",
+      visual: img5,
+    },
+    {
+      title: "Energy-efficient Lighting",
+      briefDescription:
+        "Illuminate your space responsibly. Our energy-efficient lighting solutions provide bright, sustainable, and cost-effective illumination for homes and businesses.",
+      visual: img6,
+    },
+  ],
+  callToAction:
+    "Discover the Future of Green Energy. Choose Your Sustainable Solution Today!",
+  buttonText: "Explore More",
+};
